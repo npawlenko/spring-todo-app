80f84dc359fef67c510c7e1212c0b119dad894f0
Norbert Pawlenko <blinkz8@protonmail.com>
2023-03-13 15:56:22 +0100

    Add spring security dependency and implement UserDetails interface to User

---
+++ src/main/java/com/github/npawlenko/todoapp/todoapp/models/Role.java
@@ -0,0 +1,6 @@
+package com.github.npawlenko.todoapp.todoapp.models;
+
+public enum Role {
+    USER,
+    ADMIN
+}
--- src/main/java/com/github/npawlenko/todoapp/todoapp/models/User.java
+++ src/main/java/com/github/npawlenko/todoapp/todoapp/models/User.java
@@ -1,5 +1,7 @@
 package com.github.npawlenko.todoapp.todoapp.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
@@ -7,6 +9,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Data
 @Builder
@@ -14,7 +22,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
@@ -25,4 +33,43 @@ public class User {
 
     @NotEmpty
     private char[] password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @JsonIgnore
+    @Override
+    public String getPassword() {
+        return String.valueOf(password);
+    }
+
+    @JsonProperty
+    public void setPassword(char[] password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
